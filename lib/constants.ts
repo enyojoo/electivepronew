a6e504166da624efa32e8ca20aaa374cfa9e0b4e
enyojoo <hello@enyosam.com>
2026-01-15 23:00:36 +0300

    feat: update default branding
    - Change primary color from green (#027659) to black (#000000)
    - Update logo URL to https://cldup.com/-gQTLXtXxV.svg
    - Update favicon URL to https://cldup.com/Jnah6-hWcg.png

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 // Default branding constants
-export const DEFAULT_LOGO_URL = "https://cldup.com/cE5TTe0PeC.svg"
-export const DEFAULT_FAVICON_URL = "https://cldup.com/aRNSwxLaVk.png"
-export const DEFAULT_PRIMARY_COLOR = "#027659"
+export const DEFAULT_LOGO_URL = "https://cldup.com/-gQTLXtXxV.svg"
+export const DEFAULT_FAVICON_URL = "https://cldup.com/Jnah6-hWcg.png"
+export const DEFAULT_PRIMARY_COLOR = "#000000"
 export const DEFAULT_PLATFORM_NAME = "ElectivePRO"
 export const DEFAULT_PLATFORM_DESCRIPTION = "The complete platform for managing the selection of elective courses, exchange programs, and academic pathways."
 export const DEFAULT_CONTACT_EMAIL = "support@electivepro.net"
